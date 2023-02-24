e86895991320e06e94bf49e25b65d64ca3e99e99
AcclaimedAP <alexander.w.pressfelt@gmail.com>
2023-02-24 08:07:12 +0100

    added error functionality if input is wrong

--- main.js
+++ main.js
@@ -3,14 +3,36 @@ function toggleLoginForm() {
     form.classList.toggle('hidden');
 }
 
+function Login() {
+    
+    toggleLoginForm();
+}
+
+function ThrowError() {
+    const pLoginError = document.getElementById('pLoginError');
+    pLoginError.classList.remove('hidden');
+}
+
+function attemptLogin(event) {
+    event.preventDefault();
+    const username = document.getElementById('txtInputName').value;
+    if (username) {
+        Login();
+    } else {
+        ThrowError();
+    }
+}
+
 function init() {
     const btnOpenLogin = document.getElementById('btnOpenLogin');
     const btnCloseForm = document.getElementById('btnCloseForm');
-
+    const btnLogin = document.getElementById('btnLogin');
 
 
     btnOpenLogin.addEventListener('click', toggleLoginForm);
     btnCloseForm.addEventListener('click', toggleLoginForm);
+
+    btnLogin.addEventListener('click', attemptLogin);
 }
 
 
